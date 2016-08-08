baf3f9b591d861e030580d273bfc5e114f72a8d9
Chris Peters <c.concat.p@gmail.com>
2016-08-08 16:51:54 -0700

    make == NaN, not === NaN

--- scripts/main.js
+++ scripts/main.js
@@ -57,11 +57,13 @@ import Galaxy from "./galaxy/Galaxy";
     function onMobileMenuClick () {
         let height = parseInt(menuOptions.style.height, 10);
         
-        if (height === 0 || height === NaN) {
+        if (height === 0 || height == NaN) {
             menuOptions.style.height = "100%";
         } else {
             menuOptions.style.height = "0px";
         }
+
+        //
     }
 
     function onLoad () {
